e8c235c7e524eb6c819d016b44f9de147354575a
Umair Riaz <mumair@arborgold.com>
2026-04-07 00:16:03 +0500

    PWA

--- web/src/index.js
+++ web/src/index.js
@@ -2,8 +2,25 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import './styles.css';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(<App />);
+
+// Register the service worker for PWA offline support.
+// Change register() to unregister() to disable PWA.
+serviceWorkerRegistration.register({
+  onUpdate: (registration) => {
+    const waitingServiceWorker = registration.waiting;
+    if (waitingServiceWorker) {
+      waitingServiceWorker.addEventListener('statechange', (event) => {
+        if (event.target.state === 'activated') {
+          window.location.reload();
+        }
+      });
+      waitingServiceWorker.postMessage({ type: 'SKIP_WAITING' });
+    }
+  },
+});
